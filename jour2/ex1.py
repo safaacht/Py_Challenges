5cfb139dc5d6df2c564574be3371f6fe6266c3af
safaacht <safaachtaoui@gmail.com>
2026-09-02 09:46:43 +0100

    challenge

---
+++ jour2/ex1.py
@@ -0,0 +1,34 @@
+# Manipulation fondamentale
+
+fruits = ["Pomme", "Banane", "Orange", "Fraise", "Mangue", "Kiwi"]
+
+#===========
+# === A ====
+# ==========
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(fruits)
+# print(fruits[0])    
+# print(fruits[-1])
+# print(fruits[2])
+
+#===========
+# === B ====
+# ==========
+
+# print(fruits[0:3])
+# print(fruits[-3:])    
+# print(fruits[0:len(fruits):2])  
+
+
+# ===========
+# ==== C ====
+# ===========  
+
+# for i ,fruit in enumerate(fruits) :
+#     if fruit == "Orange" :
+#         fruits[i] = "Ananas"
+        
+# print(fruits)

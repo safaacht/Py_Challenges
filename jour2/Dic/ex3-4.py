447571e826e8efbe439f59eaba474b326c1911ac
safaacht <safaachtaoui@gmail.com>
2026-09-03 10:10:24 +0100

    challenge4

--- jour2/Dic/ex3-4.py
+++ jour2/Dic/ex3-4.py
@@ -14,8 +14,30 @@ maximum = max(notes.values())
 minimum = min(notes.values())
 moyenne = somme / length  
 
-print("La meilleur note est : ", maximum)
-print("La mauvaise note est : ", minimum)
-print("La moyenne est : ", moyenne)
+# print("La meilleur note est : ", maximum)
+# print("La mauvaise note est : ", minimum)
+# print("La moyenne est : ", moyenne)
 
 
+
+
+notes_etudiants = {"Omar": 15, "Sara": 8, "Yassine": 17, "Imane": 11, "Hamza": 6, "Nadia":
+14}
+
+note_sup = {}
+note_inf = {}
+
+for student in notes_etudiants :
+    if notes_etudiants[student] < 10 :
+        note_inf[student] = notes_etudiants[student]
+    else :
+        note_sup[student] = notes_etudiants[student]    
+
+# print(note_sup)        
+
+length_note_sup = len(note_sup)
+length_note_etudiants= len(notes_etudiants)
+
+pourcentage_reussite = (length_note_sup / length_note_etudiants) * 100
+
+print(f" Pourcentage de réussite : {'%6.2f' % pourcentage_reussite} %")

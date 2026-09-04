ff18d1736310869355d325f81d8e3468c69acb36
safaacht <safaachtaoui@gmail.com>
2026-09-04 10:17:54 +0100

    displaying the docstring

--- jour3/part 3/ex1.py
+++ jour3/part 3/ex1.py
@@ -3,11 +3,15 @@
 def compute_list_sum(liste) :
 
     """ Calcule la somme des nombres pairs présents dans une liste.
-        Paramètres ---------- liste : list[int] Une liste contenant des nombres entiers. 
-        Retour ------ int La somme de tous les nombres pairs de la liste. 
-        Exemples -------- >>> compute_list_sum([1, 2, 3, 4, 5]) 6 >>> 
-        compute_list_sum([2, 6, 8]) 16 >>> 
-        compute_list_sum([1, 3, 5]) 0 """    
+
+        Paramètres 
+            ---------- liste : list[int] Une liste contenant des nombres entiers. 
+        Retour
+             ------ int La somme de tous les nombres pairs de la liste. 
+        Exemples
+             -------- >>> compute_list_sum([1, 2, 3, 4, 5]) 6 >>> 
+            compute_list_sum([2, 6, 8]) 16 >>> 
+            compute_list_sum([1, 3, 5]) 0 """    
 
     somme = 0
 
@@ -17,4 +21,6 @@ def compute_list_sum(liste) :
     print(somme)        
           
 
-compute_list_sum([1, 2, 3, 4, 6])
+# compute_list_sum([1, 2, 3, 4, 6])
+
+print(compute_list_sum.__doc__)
